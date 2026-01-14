d0fbf20b8c79406aad73c9d37f6c242ead3d8aa1
sijizhu <76112851+sijizhu@users.noreply.github.com>
2026-01-14 12:39:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,5 +1,5 @@
 ﻿import { connect } from "cloudflare:sockets";
-let config_JSON, 反代IP = '', 启用SOCKS5反代 = null, 启用SOCKS5全局反代 = false, 我的SOCKS5账号 = '', parsedSocks5Address = {};
+let config_JSON, 反代IP = 'proxyip.cmliussss.net', 启用SOCKS5反代 = null, 启用SOCKS5全局反代 = false, 我的SOCKS5账号 = '', parsedSocks5Address = {};
 let SOCKS5白名单 = ['*tapecontent.net', '*cloudatacdn.com', '*loadshare.org', '*cdn-centaurus.com', 'scholar.google.com'];
 const Pages静态页面 = 'https://edt-pages.github.io';
 ///////////////////////////////////////////////////////主程序入口///////////////////////////////////////////////
@@ -1457,3 +1457,4 @@ async function html1101(host, 访问IP) {
 </body>
 </html>`;
 }
+
